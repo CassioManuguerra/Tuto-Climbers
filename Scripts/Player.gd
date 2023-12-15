0bc40717d54f75bf17f69860e39dd78596cf1b7d
Cassio Manuguerra <91049597+CassioManuguerra@users.noreply.github.com>
2023-12-15 18:03:28 +0100

    Initial commit

---
+++ Scripts/Player.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+#player movement variables
+@export var speed = 100
+@export var gravity = 200
+
+
+#movement and physics
+func _physics_process(delta):
+	# vertical movement velocity (down)
+	velocity.y += gravity * delta
+	# horizontal movement processing (left, right)
+	horizontal_mvt()
+	#applies movement
+	move_and_slide() 
+
+func horizontal_mvt() :
+	var direction = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	velocity.x = direction * speed 
